df19ca49ac26f1aafef10b546f5d90770c7f4416
webscrapping08 <121715117+webscrapping08@users.noreply.github.com>
2023-01-01 15:18:37 +0800

    Update page.py

--- page.py
+++ page.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Onlone jobe.ph",page_icon=":tada:",layout="wide")
 st.title (':tada: Searchable PPP Loan Data :tada:')
 
 st.write('Please wait for the data . it will reload 50 Because the site . is no pagenation and randomly give data . pleasee see on site https://pppreport.org/ ')
-url= 'https://pppreport.org/advanced.php'
+url= 'https://pppreport.org'
 s =HTMLSession()
 r=s.get(url)
 
